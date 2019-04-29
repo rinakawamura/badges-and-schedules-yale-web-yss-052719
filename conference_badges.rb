7ef639c5c5a3eddb81c38f7d6a313979a6a92781
Rina Kawamura <rina.kawamura@yale.edu>
2019-04-29 04:46:12 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  messages = []
+  names.each do |name|
+    messages << badge_maker(name)
+  end
+  return messages
+end
+
+def assign_rooms(names)
+  assignments = []
+  names.each_with_index do |name, index|
+    assignments << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+  end
+  return assignments
+end
+
+def printer(names)
+  badges = batch_badge_creator(names)
+  rooms = assign_rooms(names)
+  for i in 0...badges.size
+    puts badges[i]
+  end
+  for i in 0...rooms.size
+    puts rooms[i]
+  end
+end
