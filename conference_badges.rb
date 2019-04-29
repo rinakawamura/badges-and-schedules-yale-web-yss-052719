36826311d4e402c70de2ac36996e48d1cf1427b0
Rina Kawamura <rina.kawamura@yale.edu>
2019-04-29 04:46:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ end
 def assign_rooms(names)
   assignments = []
   names.each_with_index do |name, index|
-    assignments << "Hello, #{name}! You'll be assigned to room #{index + 1}"
+    assignments << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
   end
   return assignments
 end
